f83f9a5ab7f10713abb26c41152be7db18536e4b
BensonMichael <benson.michael@anywhere.co>
2018-03-26 11:11:59 +0530

    updated jquery version and added onChange event to Search Bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-cons SearchBar = () => {
-	return <input />;
-};
+class SearchBar extends Component {
+	render() {
+		return <input onChange={ event => console.log(event.target.value) } />
+	}
+
+}
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyDzALa7iHHqN01BHp0Hgj80Ki5HQ-ygmYo';
 
-const App = () => {(
+const App = () => {
 
-	return <div>
-	<SearchBar />
-	</div>;
-	)
+	return ( 
+	<div>
+		<SearchBar />
+	</div>
+	);
 }
 
 ReactDOM.render(<App />,document.querySelector('.container'));
