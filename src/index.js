2ad32587fec5c7bb97d2c11552cadc7c92722fc9
BensonMichael <benson.michael@anywhere.co>
2018-04-17 12:56:02 +0530

    added youtube plugin and updated with video list item

--- src/index.js
+++ src/index.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React,{ Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
-
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list.js';
 const API_KEY = 'AIzaSyDzALa7iHHqN01BHp0Hgj80Ki5HQ-ygmYo';
 
-const App = () => {
 
-	return ( 
-	<div>
-		<SearchBar />
-	</div>
-	);
-}
+class App extends Component {
+
+	constructor(props){
+
+		super(props);
+
+		this.state = {videos:[]};
+
+		YTSearch({key: API_KEY, term:'dominar400'},(videos) => {
+		this.setState({videos});
+		// a.k.a this.setState({videos: videos});
+		});
+
+	}
+
+	render() {
+		return( 
+			<div>
+				<SearchBar />
+				<VideoList videos={this.state.videos} />
+			</div>
+		);
+	}
+	
+}	
 
 ReactDOM.render(<App />,document.querySelector('.container'));
